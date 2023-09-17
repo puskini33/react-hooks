ef9724e22eb637ea8d73acbe41092fb342d494f3
Elena Hirjoaba <elena.hirjoaba@stackfuel.com>
2023-09-17 20:59:57 +0200

    display pokemon last fetched and image

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,36 +2,59 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import pokemonHollow from '../fonts/pokemon/Pokemon Hollow.ttf'
+import { fetchPokemon, PokemonForm } from '../pokemon'
+import { useEffect, useState } from 'react'
 
-import {fetchPokemon, PokemonForm} from '../pokemon';
-import {useEffect, useState} from "react";
+function PokemonInfo({ pokemonName }) {
+  const [selectedPokemonDetails, setSelectedPokemonDetails] = useState({})
 
-function PokemonInfo({pokemonName}) {
+  useEffect(() => {
+    const abortController = new AbortController()
 
-    const [selectedPokemonName, setSelectedPokemonName] = useState()
+    fetchPokemon(pokemonName, abortController.signal).then((response) => {
+      if (!abortController.signal.aborted)
+        setSelectedPokemonDetails({
+          name: response.name,
+          image: response.image,
+          number: response.number,
+          attacks: response.attacks,
+          fetchedAt: response.fetchedAt,
+        })
+    })
 
-    useEffect(() => {
-        const abortController = new AbortController()
-
-        fetchPokemon(pokemonName, abortController.signal).then(response => {
-            if (!abortController.signal.aborted) setSelectedPokemonName(response.name)
-            }
-        )
-
-        return () => abortController.abort()
-
-    }, [pokemonName])
-
-
-
-    return selectedPokemonName
+    return () => abortController.abort()
+  }, [pokemonName])
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+      <div style={{ alignSelf: 'end' }}>
+        <strong>
+          <time>{selectedPokemonDetails.fetchedAt}</time>
+        </strong>
+      </div>
+      <div style={{ marginTop: '20px' }}>
+        <img
+          src={selectedPokemonDetails.image}
+          alt={selectedPokemonDetails.name}></img>
+      </div>
+      <p style={{ textTransform: 'uppercase', fontFamily: 'Georgia' }}>
+        {selectedPokemonDetails.name}
+      </p>
+    </div>
+  )
 }
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('')
-    // This is a state lift-up. this means that both children need access to the pokemonName state.
-    // why useEffect? The value of pokemonName for PokemonInfo might come also from somewhere else, not necessarily a user event
-    // (for example, it is a default value)
+
+  // This is a state lift-up. this means that both children need access to the pokemonName state.
+  // why useEffect? The value of pokemonName for PokemonInfo might come also from somewhere else, not necessarily a user event
+  // (for example, it is a default value)
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
   }
