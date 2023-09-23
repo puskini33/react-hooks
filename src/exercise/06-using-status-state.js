7b0cb34f423290679461b8341eb90649e99d626f
Elena Hirjoaba <elena.hirjoaba@stackfuel.com>
2023-09-23 19:50:36 +0200

    add alternative to set the multiple states of the component for better readability

---
+++ src/exercise/06-using-status-state.js
@@ -0,0 +1,86 @@
+// useEffect: HTTP requests
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+import {
+  fetchPokemon,
+  PokemonForm,
+  PokemonInfoFallback,
+  PokemonDataView,
+} from '../pokemon'
+import { useEffect, useState } from 'react'
+
+function PokemonInfo({ pokemonName }) {
+  const [fetchedPokemonDetails, setFetchedPokemonDetails] = useState(false)
+  const [error, setError] = useState(false)
+  const [status, setStatus] = useState('idle')
+
+  useEffect(() => {
+    if (pokemonName) {
+      const abortController = new AbortController()
+
+      setStatus('pending')
+      fetchPokemon(pokemonName, abortController.signal)
+        .then((response) => {
+          if (!abortController.signal.aborted) {
+            setFetchedPokemonDetails({
+              name: response.name,
+              image: response.image,
+              number: response.number,
+              attacks: response.attacks,
+              fetchedAt: response.fetchedAt,
+            })
+            setStatus('resolved')
+          }
+        })
+        .catch((error) => {
+          setError(error)
+          setStatus('rejected')
+        })
+
+      return () => abortController.abort()
+    }
+  }, [pokemonName])
+
+  let content
+
+  if (status === 'rejected') {
+    content = (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
+      </div>
+    )
+  } else if (status === 'idle') {
+    content = 'Submit a pokemon'
+  } else if (status === 'pending') {
+    content = <PokemonInfoFallback />
+  } else {
+    content = <PokemonDataView pokemon={fetchedPokemonDetails} />
+  }
+
+  return content
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  // This is a state lift-up. this means that both children need access to the pokemonName state.
+  // why useEffect? The value of pokemonName for PokemonInfo might come also from somewhere else, not necessarily a user event
+  // (for example, it is a default value)
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} key={pokemonName} />
+      </div>
+    </div>
+  )
+}
+
+export default App
