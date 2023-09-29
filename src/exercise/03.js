64b31ca8602ebe610f225fb5c6129fd3bf41220f
Elena Hirjoaba <elena.hirjoaba@stackfuel.com>
2023-09-29 15:56:29 +0200

    Revert "introduce serialization and deserialization to the local storage"
    This reverts commit cf6e7440a734202c2d946ee0588414795e6b79f6.

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,7 +3,7 @@
 
 import * as React from 'react'
 
-function Name({ name, onNameChange }) {
+function Name({name, onNameChange}) {
   return (
     <div>
       <label htmlFor="name">Name: </label>
@@ -12,7 +12,9 @@ function Name({ name, onNameChange }) {
   )
 }
 
+// 🐨 accept `animal` and `onAnimalChange` props to this component
 function FavoriteAnimal() {
+  // 💣 delete this, it's now managed by the App
   const [animal, setAnimal] = React.useState('')
   return (
     <div>
@@ -20,29 +22,32 @@ function FavoriteAnimal() {
       <input
         id="animal"
         value={animal}
-        onChange={(event) => setAnimal(event.target.value)}
+        onChange={event => setAnimal(event.target.value)}
       />
     </div>
   )
 }
 
-function Display({ name, animal }) {
-  return (
-    <div>{`
-    Hey ${name}, you are great! 
-    Your favourite animal is the ${animal}.
-    `}</div>
-  )
+// 🐨 uncomment this
+// function Display({name, animal}) {
+//   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+// }
+
+// 💣 remove this component in favor of the new one
+function Display({name}) {
+  return <div>{`Hey ${name}, you are great!`}</div>
 }
 
 function App() {
+  // 🐨 add a useState for the animal
   const [name, setName] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={(event) => setName(event.target.value)} />
+      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal />
-
-      <Display name={name} animal={'dog'} />
+      {/* 🐨 pass the animal prop here */}
+      <Display name={name} />
     </form>
   )
 }
